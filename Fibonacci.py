344fc2acb31697b507f073547547651ad3e7607a
Soumyadeep <soumyamyb95@gmail.com>
2021-10-23 13:36:48 +0530

    first commit

---
+++ Fibonacci.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+import time
+# import argparse
+
+# Fibonacci series without memoization
+@lru_cache
+def fib(n):
+    if n <= 2: return 1
+    return fib(n-1) + fib(n-2)
+
+# Fibonacci series with memoization
+def fib_memoized(n, mem_dict={}):
+    if n <= 2: return 1
+    if n not in mem_dict: 
+        mem_dict[n] = fib_memoized(n-1,mem_dict) + fib_memoized(n-2,mem_dict)
+    return mem_dict[n]
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--Input", help="Number to be calculated")
+    # args = parser.parse_args()
+    # # n = input("Enter number : ")
+    n = 500 #or int(args.Input)
+    start = time.time()
+    print(fib(n))
+    print("LRU_CACHE took %s seconds" % (time.time() - start))
+    start_ = time.time()
+    print(fib_memoized(n))
+    print("Manual memoization of took %s seconds" % (time.time() - start_))
